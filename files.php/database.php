3c1735b66b9c1ec475ec105f0591dc31b93da3c2
rcollo25 <71321505+rcollo25@users.noreply.github.com>
2024-06-25 19:01:44 +0200

    Update database.php

--- files.php/database.php
+++ files.php/database.php
@@ -11,13 +11,13 @@
   {
     try
     {
-      $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8'.';port=3307',
+      $db = new PDO('pgsql:host='.DB_SERVER.';dbname='.DB_NAME.';port='.DB_PORT,
         DB_USER, DB_PASSWORD);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $exception)
     {
-      error_log('Connection error: '.$exception->getMessage());
+      echo ('Connection error: '.$exception->getMessage());
       return false;
     }
     return $db;
