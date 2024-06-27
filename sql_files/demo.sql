5c64dc4ac0ddacbc9aa44a381ce2f85c9d7aec43
emma2110 <118846214+emma2110@users.noreply.github.com>
2024-06-27 19:13:01 +0200

    add script demo

---
+++ sql_files/demo.sql
@@ -0,0 +1,29 @@
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.2932636093638927, 49.84050020512298, 6, 2, 37, 1, 1, 1, 1, 1, 186, 1);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.2941793904574377, 49.840732990162316, 5, 2, 24, 1, 1, 1, 1, 1, 186, 1);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.2952675830733034, 49.84100958969832, 5, 2, 33, 1, 1, 1, 1, 1, 186, 1);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.273379794709001, 49.861408713361236, 13, 1, 160, 1, 2, 2, 1, 1, 130, 2);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.2907283252709028, 49.848521273429114, 20, 3, 415, 1, 13, 3, 1, 1, 28, 1);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.293791321750049, 49.85068279223022, 25, 12, 270, 1, 11, 4, 1, 1, 21, 1);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.291366616898525, 49.858067271741916, 6, 3, 150, 1, 148, 2, 13, 1, 215, 1);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.306323332859688, 49.83446986476363, 8, 2, 79, 1, 113, 1, 13, 1, 20, 1);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.290181032030339, 49.848650142069836, 11, 2, 80, 1, 13, 1, 1, 1, 84, 2);
+
+INSERT INTO public.arbre (longitude, latitude, haut_tot, haut_tronc, tronc_diam, id_user, id_secteur, id_stadedev, id_port, id_revetement, id_nomtech, id_feuillage) 
+VALUES (3.3073750877552475, 49.83234813665718, 5, 4, 140, 1, 113, 2, 8, 2, 211, 1);
